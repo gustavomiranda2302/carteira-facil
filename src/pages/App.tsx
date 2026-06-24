0251f24878e90ec9a496028e9c8032a2758d8087
Gustavo Miranda <137585984+gustavomiranda2302@users.noreply.github.com>
2026-06-24 00:51:10 -0300

    feat: estilos globais, variáveis CSS e componente header inicial

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,9 +1,17 @@
-
+import "../styles/utility.css"
+import "../styles/header.css"
+import "../styles/index.css"
+import "../styles/button.css"
 
 function App() {
 
+
   return (
     <>
+      <header>
+        <h1>Carteira facil</h1>
+        <button>Login</button>
+      </header>
 
     </>
   )
