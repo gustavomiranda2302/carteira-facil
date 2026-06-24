7b68955420ab554c2c9d71559cd7b71fa855a00e
Gustavo Miranda <137585984+gustavomiranda2302@users.noreply.github.com>
2026-06-24 17:07:10 -0300

    refactor: migração do CSS pra Tailwind CSS

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,17 +1,15 @@
-import "../styles/utility.css"
-import "../styles/header.css"
 import "../styles/index.css"
-import "../styles/button.css"
 
 function App() {
 
 
   return (
     <>
-      <header>
-        <h1>Carteira facil</h1>
-        <button>Login</button>
+      <header className="flex justify-between px-2 py-1 border-2 border-border bg-bg text-text h-[10vh] w-full box-border items-center">
+        <h1 className="text-2xl">Carteira facil</h1>
+        <button className="bg-cta cursor-pointer border-2 border-border text-xl rounded-md py-2 px-8 ">Login</button>
       </header>
+      <main className="bg-card min-h-screen"></main>
 
     </>
   )
