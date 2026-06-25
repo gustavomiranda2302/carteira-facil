7838c5b099bbb896d4af98eaa66d3788829ce8f9
Gustavo Miranda <137585984+gustavomiranda2302@users.noreply.github.com>
2026-06-25 01:40:48 -0300

    feat: card carousel

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,10 +9,10 @@ interface cardProps {
 
 export default function Card({ image, imagedsc, title, text }: cardProps) {
     return (
-        <div>
+        <div className="flex flex-col gap-1 bg-bg border border-border w-1/3 h-1/1">
             <img src={image} alt={imagedsc} />
-            <h3>{title}</h3>
-            <p>{text}</p>
+            <h3 className="text-2xl text-text ">{title}</h3>
+            <p className="text-md text-text2">{text}</p>
         </div>
     )
 
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,6 +1,7 @@
 import "../styles/index.css"
 import wallet from "../assets/wallet.svg"
 import { useNavigate } from "react-router-dom"
+import Card from "../components/Card";
 
 function App() {
   const navigate = useNavigate();
@@ -12,20 +13,51 @@ function App() {
   return (
     <>
       <header className="flex justify-between px-2 py-1 border-2 border-border bg-bg text-text h-[10vh] w-full  items-center overflow-hidden">
-        <div className=" flex items-center  border border-amber-400 w-1/2 h-1/1 py-3 box-border">
+        <div className=" flex items-center   w-1/2 h-1/1 py-3 box-border">
           <img src={wallet} alt="icone-carteira" />
           <h1 className="text-2xl">Carteira facil</h1>
         </div >
-        <div className=" flex px-4 border border-red-500 w-1/2 h-12/12 justify-end items-center gap-2">
+        <div className=" flex px-4 w-1/2 h-12/12 justify-end items-center gap-2">
           <a href="#footer">
             <button className="bg-cta cursor-pointer border border-border text-md rounded-lg py-2 px-8 w-32 h-14 text-center ">Sobre</button>
           </a>
-          <button className="bg-cta cursor-pointer border border-border text-md rounded-lg py-2 px-8 w-32 h-14 text-center " onClick={navigateToLogin}>Login</button>
+          <button className="bg-cta cursor-pointer border border-border text-md rounded-lg py-2 px-8 w-32 h-14 text-center hover:bg-accent " onClick={navigateToLogin}>Login</button>
+        </div>
+      </header>
+
+      <main className="flex flex-col bg-card min-h-screen py-5 items-center gap-y-4">
+        <h1 className="text-5xl text-text">Sua carteira inteligente, do jeito que você merece</h1>
+        <h2 className="text-3xl text-text2">Investir na B3 nunca foi tão facil</h2>
+
+
+
+        <div className=" flex border-2 border-red-500 min-h-[10%] w-fit overflow-hidden">
+
+
+
+          <div className="flex animate-swipe hover-animate-stop shrink-0 flex-row w-max">
+            <div className="flex shrink-0 min-w-full">
+
+              <Card image={wallet} title="teste" text="teste texto" />
+              <Card image={wallet} title="teste" text="teste texto" />
+              <Card image={wallet} title="teste" text="teste texto" />
+
+            </div>
+            <div className="flex shrink-0 min-w-full" aria-hidden="true">
+              <Card image={wallet} title="teste" text="teste texto" />
+              <Card image={wallet} title="teste" text="teste texto" />
+              <Card image={wallet} title="teste" text="teste texto" />
+
+
+            </div>
+
+          </div>
+
 
         </div>
 
-      </header>
-      <main className="bg-card min-h-screen"></main>
+
+      </main>
       <footer id="footer" className=" bg-bg min-h-10 " ><p>teste</p></footer>
 
     </>
