3250507d33cee7e9adf33453710b9b870ef60da5
Gustavo Miranda <137585984+gustavomiranda2302@users.noreply.github.com>
2026-06-25 23:10:17 -0300

    feat: layout tela de login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,21 @@
+import "../styles/index.css";
 
 export default function Login() {
     return (
-        <div id="teste">
-
+        <div className=" w-full min-h-screen flex flex-col">
+            <div className="w-full h-[10vh] bg-bg ">
+                <p className="text-white">a</p>
+            </div>
+            <div className="w-full h-[80vh] bg-border flex flex-col justify-center items-center">
+                <div className="bg-white w-1/3 h-2/3 flex flex-col items-center pt-4 shadow-bg" >
+                    <p>a</p>
+                </div>
+            </div>
+            <div className="bg-bg">
+                <p className="text-white w-full h-[10vh]">a</p>
+            </div>
         </div>
+
 
     )
 }
